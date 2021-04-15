0791e3ab5ce700c1cfac46eb2c3fa826a970573d
Rio <riostockton@gmail.com>
2021-04-15 18:50:21 +0100

    revert prior changes

--- auth/firebaseClient.ts
+++ auth/firebaseClient.ts
@@ -2,11 +2,11 @@ const firebase = require("firebase");
 
 const FIREBASE_CONFIG = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 
 export default function firebaseClient() {
--- utils/shopify.js
+++ utils/shopify.js
@@ -2,6 +2,6 @@ import Client from "shopify-buy";
 
 // Initialize a client to return content in the store's primary
 export const client = Client.buildClient({
-  domain: "book-store-55.myshopify.com",
-  storefrontAccessToken: "b818f747b09c5737c1aa9f6e30cbcebc"
+  domain: process.env.SHOPIFY_DOMAIN,
+  storefrontAccessToken: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN
 });
