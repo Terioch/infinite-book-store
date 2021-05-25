ddf36beb7d56fd7e54fa4194835a28f63a6677ed
Rio <riostockton@gmail.com>
2021-05-25 16:07:04 +0100

    Implemented first design iteration for an individual cart item

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,15 +1,44 @@
 import React from "react";
 import { LineItem } from "../models/Cart";
-import { Header } from "semantic-ui-react";
+import { Divider, Header, List, Grid, Image } from "semantic-ui-react";
+import cartStyles from "../styles/Cart.module.css";
+
+const { Item } = List;
+const { Row, Column } = Grid;
 
 interface Props {
 	item: LineItem;
 }
 
 const CartItem: React.FC<Props> = ({ item }) => {
+	const {
+		title,
+		quantity,
+		variant: { price, image },
+	} = item;
+
 	return (
 		<>
-			<Header as="h3">{item.title}</Header>
+			<Grid columns={2} padded="vertically" centered relaxed>
+				<Row>
+					<Column width={4}>
+						<Image src={image.src} size="tiny" centered />
+					</Column>
+					<Column width={6} verticalAlign="middle" textAlign="left">
+						<Header as="h3" color="blue">
+							{title}
+						</Header>
+						<List horizontal>
+							<Item>
+								<Header as="h3">£{price}</Header>
+							</Item>
+							<Item>
+								<Header as="h3">Quantity: {quantity}</Header>
+							</Item>
+						</List>
+					</Column>
+				</Row>
+			</Grid>
 		</>
 	);
 };
